0c6bdf0e72268491531c44910b0f97679f739610
pereiramemo <pereiramemo@gmail.com>
2017-11-15 17:43:59 +0100

    updated: added models new version

--- R/bgc_class_model.R
+++ R/bgc_class_model.R
@@ -108,12 +108,12 @@ class_model_train <- function(y,
     # remove only zero columns ------------------------------------------------
 
     predictors_present_train <- predictors_present_train[, colSums(predictors_present_train) > 0, drop =  F]
-    
+
     if ( is_empty(predictors_present_train) == T ) {
       warning("not enough predictor values")
       return(NULL)
     }
-    
+
     if ( is_empty(predictors_present_train) == F ) {
     # train binary model ------------------------------------------------------
 
@@ -126,7 +126,7 @@ class_model_train <- function(y,
                                 replace = T,
                                 nodesize = 10)
       }
-      
+
       if (binary_method == "svm") {
         predictors_train <- predictors_train[ ,colSums(predictors_present_train) > 0, drop = FALSE]
         model_c <- svm(factor(response_binary_train) ~ ., data = predictors_train )
@@ -135,11 +135,11 @@ class_model_train <- function(y,
       # train regression model --------------------------------------------------
 
       set.seed(seed = seed)
-      
+
       if (regression_method == "lm") {
         model_r <- lm(response_present_train ~ ., data = predictors_present_train)
       }
-      
+
       if (regression_method == "rf") {
         model_r <- randomForest(response_present_train ~ . ,
                                 data = predictors_present_train,
@@ -147,7 +147,7 @@ class_model_train <- function(y,
                                 replace = T,
                                 nodesize = 1)
       }
-      
+
       if (regression_method == "svm") {
         predictors_present_train <- predictors_present_train[ ,colSums(predictors_present_train) > 0, drop = FALSE]
         model_r <- svm(response_present_train ~ ., data = predictors_present_train)
@@ -157,8 +157,8 @@ class_model_train <- function(y,
                       binary_model = model_c,
                       regression_model = model_r)
       return.list <-  results
-    
-    }    
+
+    }
   }
   return(return.list)
 }
@@ -222,7 +222,7 @@ class_model_predict <- function(x,
 #' @examples
 #' wrap_up_predict(x)
 
-wrap_up_predict <- function(x,m = models_list) {
+wrap_up_predict <- function(x,m = models_list_oms) {
 
   pred <- list()
   used_models <- list()
